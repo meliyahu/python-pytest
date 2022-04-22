e6da5a183fece778d009c07616ffb6bdbe506281
Mosheh EliYahu <mosheh.eliyahu@gmail.com>
2022-04-22 17:21:53 +0930

    updates

--- tests/test_temp_path.py
+++ tests/test_temp_path.py
@@ -12,7 +12,7 @@ def create_audio_files(tmp_path):
     """create list of files"""
     d_dir = tmp_path / "sub"
     d_dir.mkdir()
-    audio_types = ['file1.wac', 'file1.wav', 'file1.flac']
+    audio_types = ["file1.wac", "file1.wav", "file1.flac"]
     audio_file_list = []
     for audio_type in audio_types:
         p_file = d_dir / audio_type
@@ -22,10 +22,31 @@ def create_audio_files(tmp_path):
     return audio_file_list
 
 
+@pytest.fixture(name="audio_config_fixture")
+def gen_audio_config(tmp_path):
+    """Generate audio config fixture"""
+    config = {
+        "base_source": tmp_path / "data/users",
+        "base_target": tmp_path / "data/audio",
+        "base_spectogram": tmp_path / "data/vis",
+        "base_archive": tmp_path / "data/archive",
+        "base_issues": tmp_path / "data/issues",
+        "site_name": "alic",
+        "enabled": True,
+        "process_duplicates_published": True,
+        "process_duplicates_source": True,
+        "sensors": [
+            {"source": "TTE/Sensor_ALIC01", "target": "tte-alic01"},
+            {"source": "ASM/Sensor_Alic02", "target": "asm-alic02"},
+        ],
+    }
+    return config
+
 
 class TestTempPath:
     """Test class"""
-    def test_create_file(self, audio_files_fixture):
+
+    def test_create_file(self, audio_files_fixture, audio_config_fixture):
         """Create a file"""
 
         # d_dir = tmp_path / "sub"
@@ -39,4 +60,5 @@ class TestTempPath:
         # assert 0
         for file in audio_files_fixture:
             assert Path(file).is_file()
-        # print (create_audio_files)
+
+        print(audio_config_fixture)
